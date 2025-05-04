3999c958bc61ac51c2b004c696a77fda5bed95ba
Philippe Blanchette <philippe.blanchette@nventive.com>
2025-05-04 19:19:37 -0400

    Initial Commit

---
+++ Repository-pattern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Repository-pattern
+//
+//  Created by Philippe Blanchette on 2025-05-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Repository-pattern/Repository_patternApp.swift
@@ -0,0 +1,17 @@
+//
+//  Repository_patternApp.swift
+//  Repository-pattern
+//
+//  Created by Philippe Blanchette on 2025-05-04.
+//
+
+import SwiftUI
+
+@main
+struct Repository_patternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
